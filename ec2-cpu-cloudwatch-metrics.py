854e81bd1b24db57b45e4f8528c2719e2c41c4be
Abhishek Vasisht <abhishek.vasisht@hdsnz.int>
2021-07-14 14:18:21 +1200

    Adding Cloudwatch Metrics Python Script

---
+++ ec2-cpu-cloudwatch-metrics.py
@@ -0,0 +1,62 @@
+import boto3
+# import os
+# import json
+import sys
+from datetime import datetime, timedelta
+
+cProfile = sys.argv[1]
+mProfile = sys.argv[2]
+
+sessionClient = boto3.session.Session(profile_name = cProfile)
+sessionMaster = boto3.session.Session(profile_name = mProfile)
+ec2 = sessionClient.resource('ec2')
+
+# ec2 = boto3.resource('ec2')
+# def lambda_handler(event, context):
+############## Grabbing the Running EC2 Instances Id's from the Client Account     
+instances = ec2.instances.filter(
+    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+for instance in instances:
+    # print(instance.id, instance.instance_type)
+    # print (datetime.utcnow() - timedelta(seconds = 600))
+    # print (datetime.utcnow())
+############## Getting the CPU Utilization CloudWatch Metrics for EC2 Instances from the Client Account
+    cloudwatchClient = sessionClient.client('cloudwatch')
+    getRequest = cloudwatchClient.get_metric_statistics(
+            Namespace="AWS/EC2",
+            MetricName="CPUUtilization",
+            Dimensions=[
+            {
+                'Name': 'InstanceId',
+                'Value': instance.id
+            }
+            ],
+            StartTime=datetime.utcnow() - timedelta(days = 14),
+            EndTime=datetime.utcnow(),
+            Period=300,
+            Statistics=['Average']
+            )
+############### Processing and filtering the data
+    filterResponse = getRequest.get('Datapoints',[])
+    print (filterResponse)
+    cloudwatchMaster = sessionMaster.client('cloudwatch')
+    # print (dic['Timestamp'])
+############### Pushing the Custom CloudWatch Metric to the Master Account
+    for dic in filterResponse:
+        putResponse = cloudwatchMaster.put_metric_data(
+            MetricData=[
+                {
+                    'MetricName': 'CPUUtilization',
+                    'Dimensions': [
+                        {
+                        'Name':'InstanceId',
+                        'Value': instance.id
+                        }
+                    ],
+                    'Value': dic['Average'],
+                    'Timestamp': dic['Timestamp']
+                }
+            ],
+            Namespace = 'Custom/EC2'
+        )
+        print (putResponse)
