054845535207fc7b0b5d556ef122a3dfd4eca701
Abhishek Vasisht <abhishek.vasisht@leaven.co.nz>
2021-07-14 14:47:12 +1200

    Removed print statements from the code

--- ec2-cpu-cloudwatch-metrics.py
+++ ec2-cpu-cloudwatch-metrics.py
@@ -11,15 +11,10 @@ sessionClient = boto3.session.Session(profile_name = cProfile)
 sessionMaster = boto3.session.Session(profile_name = mProfile)
 ec2 = sessionClient.resource('ec2')
 
-# ec2 = boto3.resource('ec2')
-# def lambda_handler(event, context):
 ############## Grabbing the Running EC2 Instances Id's from the Client Account     
 instances = ec2.instances.filter(
     Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 for instance in instances:
-    # print(instance.id, instance.instance_type)
-    # print (datetime.utcnow() - timedelta(seconds = 600))
-    # print (datetime.utcnow())
 ############## Getting the CPU Utilization CloudWatch Metrics for EC2 Instances from the Client Account
     cloudwatchClient = sessionClient.client('cloudwatch')
     getRequest = cloudwatchClient.get_metric_statistics(
@@ -40,7 +35,6 @@ for instance in instances:
     filterResponse = getRequest.get('Datapoints',[])
     print (filterResponse)
     cloudwatchMaster = sessionMaster.client('cloudwatch')
-    # print (dic['Timestamp'])
 ############### Pushing the Custom CloudWatch Metric to the Master Account
     for dic in filterResponse:
         putResponse = cloudwatchMaster.put_metric_data(
@@ -59,4 +53,3 @@ for instance in instances:
             ],
             Namespace = 'Custom/EC2'
         )
-        print (putResponse)
